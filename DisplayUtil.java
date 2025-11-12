38bcc3f30e4239498839a8b491fefd32c311a8a3
bomnorng.soeurt <soeurtbomnorng3@gmail.com>
2025-11-12 22:46:05 +0700

    first commit

---
+++ DisplayUtil.java
@@ -0,0 +1,13 @@
+package student_management_system;
+
+public class DisplayUtil {
+    public final static String MENU = """
+            [ Student Management System ]
+            1. List student
+            2. Search student (id, name, gender, score)
+            3. Add new student
+            4. Update student by id
+            5. Delete student by id
+            0. Exit application
+            """;
+}
